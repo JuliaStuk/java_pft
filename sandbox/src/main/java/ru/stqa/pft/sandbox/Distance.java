f294717793f7e8f8d4f51c6f9269de78946f3d67
JuliaStuk <51078301+JuliaStuk@users.noreply.github.com>
2019-12-25 23:36:27 +0300

    Homework 2 (point)

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+public class Distance {
+    public static void main(String[] args) {
+
+        Point p1 = new Point(2, 1);
+        Point p2 = new Point(6, 4);
+
+        System.out.println("Расстояние между точкой А с координатой (" + p1.x + ", " + p1.y + ") и " +
+                "точкой В с координатой (" + p2.x + ", " + p2.y + ") = " +
+                distance(p1, p2));
+
+    }
+
+    public static double distance(Point p1, Point p2)
+    {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    }
+}
