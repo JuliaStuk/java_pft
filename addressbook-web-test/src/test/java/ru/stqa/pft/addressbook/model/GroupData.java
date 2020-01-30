cc722f4db7ce4235511f3c01486daed99752441d
JuliaStuk <51078301+JuliaStuk@users.noreply.github.com>
2020-01-30 16:24:01 +0300

    Поиск максимального элемента

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,10 +3,24 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
+
+    public GroupData(String name, String header, String footer) {
+        this.id = 0;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public GroupData(int id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -30,21 +44,12 @@ public class GroupData {
                 '}';
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public GroupData(String name, String header, String footer) {
-        this.id = null;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
-    }
-    public GroupData(String id, String name, String header, String footer) {
+    public void setId(int id) {
         this.id = id;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
     }
 
     public String getName() {
